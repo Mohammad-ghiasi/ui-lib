da68fbe754978435ab88169b43264178c1b6fb34
Mohammad-ghiasi <mohammadghiasi005@gmail.com>
2025-04-16 19:50:48 +0330

    ui-lib_1.1.0

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import { Button, ButtonProps } from './Button';
+// import { Button, ButtonProps } from './Button';
 
 const meta: Meta<ButtonProps> = {
   title: 'Components/Button',
